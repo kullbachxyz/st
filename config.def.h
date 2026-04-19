fa3b0beef6bd464c193b69ab8e745481f064a1f8
kullbachxyz <ph@kullbach.net>
2026-04-19 23:14:41 +0200

    font support added

--- config.def.h
+++ config.def.h
@@ -5,8 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 0;
+static char *font = "monospace:size=12";
+static char *font2[] = {
+    "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
+};static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
