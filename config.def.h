90d0e36c78d787a8bdabcfd6000129c1ebd89bf5
kullbachxyz <ph@kullbach.net>
2026-08-13 10:58:04 +0200

    alpha fix

--- config.def.h
+++ config.def.h
@@ -172,6 +172,7 @@ typedef struct {
 
 static ResourcePref resources[] = {
 	{ "font",        STRING,  &font },
+	{ "alpha",       FLOAT,   &alpha },
 	{ "color0",      STRING,  &colorname[0] },
 	{ "color1",      STRING,  &colorname[1] },
 	{ "color2",      STRING,  &colorname[2] },
